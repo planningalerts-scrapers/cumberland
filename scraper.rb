5bcfc41915cc4d78fce4f6e8fb86fbe44764c69e
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 13:47:36 +1000

    No need for comment_url

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,6 @@ require 'mechanize'
 require 'date'
 
 base_url    = "http://eplanning.cumberland.nsw.gov.au/Pages/XC.Track/"
-comment_url = "mailto:council@cumberland.nsw.gov.au"
 
 time = Time.new
 
@@ -30,7 +29,6 @@ results.each do |result|
   record['address']           = result.text.split("Address:")[1].split("[More]")[0].gsub("\r", " ").gsub("\n", " ").squeeze(' ').strip
   record['description']       = result.text.split("\r\n")[3].squeeze(' ').strip
   record['info_url']          = base_url + result.css('a')[0]['href'].split("/Pages/XC.Track/")[1]
-  record['comment_url']       = comment_url
   record['date_scraped']      = Date.today.to_s
   record['date_received']     = Date.strptime(result.text.split("Lodged:")[1].split("Applicant")[0].squeeze("\r\n ").strip, '%d/%m/%Y').to_s
 
