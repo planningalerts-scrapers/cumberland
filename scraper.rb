ae5225e8f9b3c879164e0c08868d945188fdcbab
Matthew Landauer <mlandauer@gmail.com>
2019-06-09 12:55:11 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,3 @@
 require "icon_scraper"
 
-IconScraper.scrape_with_params(
-  url: "http://eplanning.cumberland.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
-  period: "last14days"
-) do |record|
-  IconScraper.save(record)
-end
+IconScraper.scrape_and_save(:cumberland)
