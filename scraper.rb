bac7ab0d18f6c3494f2fdb4eb5c23b8b30c81e05
Matthew Landauer <mlandauer@gmail.com>
2019-06-09 12:50:46 +1000

    Switch over to using xml feed

--- scraper.rb
+++ scraper.rb
@@ -1,28 +1,10 @@
-require 'scraperwiki'
-require 'mechanize'
-require 'date'
+require "icon_scraper"
 
-time = Time.new
+IconScraper.scrape_with_params(
+  url: "http://eplanning.cumberland.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
+  period: "last14days"
+) do |record|
+  record["address"] = record["address"].gsub(",", "")
 
-base_url    = "http://eplanning.cumberland.nsw.gov.au/Pages/XC.Track/"
-page_url = base_url + "SearchApplication.aspx?k=LodgementDate&t=&d=last14days"
-
-agent   = Mechanize.new
-page    = agent.get(page_url)
-results = page.css("div[id='hiddenresult'] [class='result']")
-
-# Extract application'ss
-results.each do |result|
-  record = {}
-  record['council_reference'] = result.css('a').text.split("\r\n")[0]
-  record['description']       = result.text.split("\r\n")[3].squeeze(' ').strip
-  record['date_received']     = Date.strptime(result.text.split("Lodged:")[1].split("Applicant")[0].squeeze("\r\n ").strip, '%d/%m/%Y').to_s
-  record['address']           = result.text.split("Address:")[1].split("[More]")[0].gsub("\r", " ").gsub("\n", " ").squeeze(' ').strip
-  record['date_scraped']      = Date.today.to_s
-  record['info_url']          = base_url + result.css('a')[0]['href'].split("/Pages/XC.Track/")[1]
-
-  # Saving data to DB
-  puts "Saving record " + record['council_reference'] + ", " + record['address']
-  # puts record
-  ScraperWiki.save_sqlite(['council_reference'], record)
+  IconScraper.save(record)
 end
